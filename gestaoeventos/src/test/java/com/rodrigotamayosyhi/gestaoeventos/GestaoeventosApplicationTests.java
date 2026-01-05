4992aaf0e44d79f1d03b071078ab5d6cd3248ed0
rodrigo-tamayoshi <rodrigo.tamayoshi@gmail.com>
2026-01-05 19:03:33 -0300

    Commit inicial

---
+++ gestaoeventos/src/test/java/com/rodrigotamayosyhi/gestaoeventos/GestaoeventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigotamayosyhi.gestaoeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoeventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
